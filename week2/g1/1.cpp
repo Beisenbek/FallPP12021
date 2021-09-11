76635bf23432cd225b2cedf5c48884b48f161a8e
Beisenbek Baisakov <bbaisakov@dar.kz>
2021-09-11 14:12:51 +0600

    upd

---
+++ week2/g1/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char c, c2;
+
+    cin >> c;
+    cin >> c2;
+
+    cout << c2 << endl;
+    cout << c << endl; 
+
+    return 0;
+}
---
+++ week2/g1/3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int c;
+
+    cin >> c;
+
+    char x = char(c);
+
+    cout << x << endl; 
+
+    return 0;
+}
